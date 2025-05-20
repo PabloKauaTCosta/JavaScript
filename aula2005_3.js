6746ac0f0a9d7f7ecb445e9cfab21d30b0ac0019
Kauã Costa <alaokaua@gmail.com>
2025-05-20 16:55:53 -0300

    Add files via upload

---
+++ aula2005_3.js
@@ -0,0 +1,6 @@
+metroquadrado = function(){
+    altura = prompt("Digite a altura: ")
+    comprimento = prompt("Digite o cmprimento: ")
+    metroquadrado = altura * comprimento
+    document.write("Metro quadrado = " + metroquadrado)
+}
