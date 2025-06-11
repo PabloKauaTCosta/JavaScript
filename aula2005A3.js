c6e7ccb79573eef188c97d86ec560b100ef89aee
Kauã Costa <alaokaua@gmail.com>
2025-06-11 14:21:57 -0300

    Delete aula2005A3.js

--- aula2005A3.js
+++
@@ -1,6 +0,0 @@
-metroquadrado = function(){
-    altura = prompt("Digite a altura: ")
-    comprimento = prompt("Digite o cmprimento: ")
-    metroquadrado = altura * comprimento
-    document.write("Metro quadrado = " + metroquadrado)
-}
